ea8c1a7ff9484dc12755b621c44e57948158be80
Victoria <vperr007@fiu.edu>
2017-08-06 14:55:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var quote;
 var author;
 function getNewQuote() {
     $.ajax({
-        url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en',
+        url: 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en',
         jsonp: 'jsonp',
         dataType: 'jsonp',
         success: function (response) {
